c90eb887a239e3b205013aaeb364ecb4dfcd9e9b
Arnold Zokas <arnold.zokas@coderoom.net>
2013-06-10 14:23:23 +0100

    Implemented basic HttpHandler.

---
+++ Piccolo/HttpHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Web;
+
+namespace Piccolo
+{
+	public class HttpHandler : IHttpHandler
+	{
+		public void ProcessRequest(HttpContext context)
+		{
+			context.Response.Write("OK");
+		}
+
+		public bool IsReusable
+		{
+			get { return true; }
+		}
+	}
+}
