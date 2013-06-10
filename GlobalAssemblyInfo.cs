54d9927d6928796b901949218b35dc307ff9daa9
Arnold Zokas <arnold.zokas@coderoom.net>
2013-06-10 14:14:24 +0100

    Added GlobalAssemblyInfo.

---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("OpenTable")]
+[assembly: AssemblyProduct("Piccolo")]
+[assembly: AssemblyCopyright("Copyright © OpenTable 2013")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- Piccolo.Samples/Properties/AssemblyInfo.cs
+++ Piccolo.Samples/Properties/AssemblyInfo.cs
@@ -1,10 +1,3 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Piccolo")]
-[assembly: AssemblyCompany("OpenTable")]
-[assembly: AssemblyProduct("Piccolo")]
-[assembly: AssemblyCopyright("Copyright © OpenTable 2013")]
-[assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
