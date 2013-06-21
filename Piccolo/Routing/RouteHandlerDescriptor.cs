a00a052f9b1532fe5a8c8e004e34afeaa283760c
Arnold Zokas <arnold.zokas@coderoom.net>
2013-06-21 18:20:00 +0100

    'Output' is no longer a reserved property name.

--- Piccolo/Routing/RouteHandlerDescriptor.cs
+++ Piccolo/Routing/RouteHandlerDescriptor.cs
@@ -6,8 +6,6 @@ namespace Piccolo.Routing
 {
 	public static class RouteHandlerDescriptor
 	{
-		private static readonly string[] _reservedPropertyNames = new[] {"output"};
-
 		public static List<RouteAttribute> GetRouteAttributes(Type requestHandler)
 		{
 			return requestHandler.GetCustomAttributes(typeof(RouteAttribute), true).Cast<RouteAttribute>().ToList();
@@ -18,10 +16,7 @@ namespace Piccolo.Routing
 			if (requestHandler == null)
 				return new Dictionary<string, Type>();
 
-			var allProperties = requestHandler.GetProperties();
-			var allInputProperties = allProperties.Where(x => _reservedPropertyNames.Contains(x.Name.ToLower()) == false);
-
-			return allInputProperties.ToDictionary(x => x.Name.ToLower(), x => x.PropertyType);
+			return requestHandler.GetProperties().ToDictionary(x => x.Name.ToLower(), x => x.PropertyType);
 		}
 
 		public static string GetVerb(Type requestHandler)
