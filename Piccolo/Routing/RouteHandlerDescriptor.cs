90e24dddfbba772fb9d2b87593095056f383d4bd
Arnold Zokas <arnold.zokas@coderoom.net>
2013-06-21 18:26:26 +0100

    Refactored RouteHandlerDescriptor.

--- Piccolo/Routing/RouteHandlerDescriptor.cs
+++ Piccolo/Routing/RouteHandlerDescriptor.cs
@@ -6,6 +6,13 @@ namespace Piccolo.Routing
 {
 	public static class RouteHandlerDescriptor
 	{
+		private static readonly Dictionary<string, string> _requestHandlerVerbMap = new Dictionary<string, string>
+			{
+				{typeof(IGet<>).Name, "get"},
+				{typeof(IPut<>).Name, "put"},
+				{typeof(IPost<>).Name, "post"}
+			};
+
 		public static List<RouteAttribute> GetRouteAttributes(Type requestHandler)
 		{
 			return requestHandler.GetCustomAttributes(typeof(RouteAttribute), true).Cast<RouteAttribute>().ToList();
@@ -22,15 +29,12 @@ namespace Piccolo.Routing
 		public static string GetVerb(Type requestHandler)
 		{
 			var interfaces = requestHandler.GetInterfaces();
+			var match = _requestHandlerVerbMap.SingleOrDefault(pair => interfaces.Any(x => x.Name == pair.Key));
 
-			if (interfaces.Any(x => x.Name == typeof(IGet<>).Name))
-				return "get";
-			if (interfaces.Any(x => x.Name == typeof(IPut<>).Name))
-				return "put";
-			if (interfaces.Any(x => x.Name == typeof(IPost<>).Name))
-				return "post";
-			else
+			if (match.Key == null)
 				throw new InvalidOperationException(string.Format("Request handler [{0}] does not implement any of the following supported interfaces: IGet<>, IPut<>, IPost<>.", requestHandler.FullName));
+
+			return match.Value;
 		}
 	}
 }
