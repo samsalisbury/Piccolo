2856f2cb3267d3687c8a9030655f98c57409ac06
Arnold Zokas <arnold.zokas@coderoom.net>
2013-06-21 18:41:05 +0100

    Refactored RouteHandlerLookupNode.

--- Piccolo/Routing/RouteHandlerLookupNode.cs
+++ Piccolo/Routing/RouteHandlerLookupNode.cs
@@ -12,38 +12,40 @@ namespace Piccolo.Routing
 		{
 		}
 
-		internal RouteHandlerLookupNode(string headFragment, IList<string> routeTemplateFragments, Type requestHandler)
+		internal RouteHandlerLookupNode(string headFragment, IEnumerable<string> routeTemplateFragments, Type requestHandler)
 		{
 			ChildNodes = new List<RouteHandlerLookupNode>();
 			IsStaticRouteTemplateFragment = IsStaticFragment(headFragment);
+			IsVirtualRouteTemplateFragment = IsVirtualFragment(routeTemplateFragments);
 			RouteTemplateFragment = RemoveDynamicFragmentTokens(headFragment);
+			RequestHandler = requestHandler;
 
-			if (routeTemplateFragments.Count == 0) // is not a virtual fragment
-			{
-				RequestHandler = requestHandler;
+			if (IsVirtualRouteTemplateFragment == false)
 				RequestHandlerProperties = RouteHandlerDescriptor.GetRequestHandlerProperties(requestHandler);
-			}
 		}
 
 		internal IList<RouteHandlerLookupNode> ChildNodes { get; set; }
+
 		internal bool IsStaticRouteTemplateFragment { get; set; }
+
+		internal bool IsVirtualRouteTemplateFragment { get; set; }
+
 		internal string RouteTemplateFragment { get; set; }
+
 		internal Type RequestHandler { get; set; }
+
 		internal Dictionary<string, Type> RequestHandlerProperties { get; set; }
 
 		internal void AddNode(IList<string> routeTemplateFragments, Type requestHandler)
 		{
 			var headFragment = routeTemplateFragments.First();
-
-			var childNode = FindChildNode(headFragment);
 			var remainingTemplateFragments = routeTemplateFragments.Skip(1).ToList();
 
+			var childNode = FindChildNode(headFragment);
 			if (childNode != null)
 			{
-				if (remainingTemplateFragments.Count > 0)
-				{
+				if (remainingTemplateFragments.Any())
 					childNode.AddNode(remainingTemplateFragments, requestHandler);
-				}
 				else
 				{
 					childNode.RequestHandler = requestHandler;
@@ -61,12 +63,18 @@ namespace Piccolo.Routing
 
 		private RouteHandlerLookupNode FindChildNode(string headFragment)
 		{
-			return ChildNodes.SingleOrDefault(x => x.RouteTemplateFragment == RemoveDynamicFragmentTokens(headFragment));
+			var searchTerm = RemoveDynamicFragmentTokens(headFragment);
+			return ChildNodes.SingleOrDefault(x => x.RouteTemplateFragment == searchTerm);
 		}
 
 		private static bool IsStaticFragment(string headFragment)
 		{
 			return headFragment.Contains('{') == false;
+		}
+
+		private static bool IsVirtualFragment(IEnumerable<string> routeTemplateFragments)
+		{
+			return routeTemplateFragments.Any();
 		}
 
 		private static string RemoveDynamicFragmentTokens(string headFragment)
