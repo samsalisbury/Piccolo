5785d2c2aab95a638e44a1d6c850bef5a427f342
Arnold Zokas <arnold.zokas@coderoom.net>
2013-08-12 14:57:06 +0100

    Created build script (closes #3).

---
+++ build/build.ps1
@@ -0,0 +1,37 @@
+framework "4.0"
+
+properties {
+	$build_dir = split-path $psake.build_script_file
+	$project_dir = "$build_dir\.."
+
+	$solution_file = "$project_dir\Piccolo.sln"
+	$build_configuration = "Release"
+	$build_output_dir = "$project_dir\build-output"
+}
+
+task default -depends compile
+
+formatTaskName {
+	param($taskName)
+	write-host
+	write-host (("-"*30) + "[ " + $taskName.ToUpper() + " ]" + ("-"*30)) -fore Green
+	write-host
+}
+
+taskSetup {
+	write-output "##teamcity[progessMessage '$($psake.context.Peek().currentTaskName.ToUpper())']"
+}
+
+task compile -depends clean {
+	try {
+		exec {
+			msbuild $solution_file /m /property:"Configuration=$build_configuration;OutputPath=$build_output_dir" /nologo
+		}
+	} catch {
+		write-output "##teamcity[buildStatus status='FAILURE' ]"
+	}
+}
+
+task clean {
+	remove-item -recurse -force $build_output_dir
+}
