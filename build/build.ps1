50b3976d0e824cb73cd3d6381fa39e265a76eb9a
Arnold Zokas <arnold.zokas@coderoom.net>
2013-08-13 14:43:38 +0100

    Implemented build-time assembly versioning.

--- build/build.ps1
+++ build/build.ps1
@@ -3,10 +3,16 @@ framework "4.0"
 properties {
 	$build_dir = split-path $psake.build_script_file
 	$project_dir = "$build_dir\.."
-
-	$solution_file = "$project_dir\Piccolo.sln"
-	$build_configuration = "Release"
 	$build_output_dir = "$project_dir\build-output"
+	
+	$solution_file = "$project_dir\Piccolo.sln"
+	$assembly_info_file = "$project_dir\GlobalAssemblyInfo.cs"
+	
+	$build_configuration = "Release"
+	$version_major = 0;
+	$version_minor = 1;
+	$version_build = 999;
+	$version_revision = 0;
 }
 
 task default -depends compile
@@ -22,8 +28,21 @@ taskSetup {
 	write-output "##teamcity[progessMessage '$($psake.context.Peek().currentTaskName.ToUpper())']"
 }
 
-task compile -depends clean {
+task compile -depends set-version-number {
 	exec { msbuild $solution_file /m /property:"Configuration=$build_configuration;OutputPath=$build_output_dir" /nologo }
+}
+
+task set-version-number -depends clean {
+	$version = "$version_major.$version_minor.$version_build.$version_revision"
+	write-host Setting assembly version to $version
+	
+	$versionPattern = 'AssemblyVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)'
+	$versionAssembly = 'AssemblyVersion("' + $version + '")';
+	$versionFilePattern = 'AssemblyFileVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)'
+	$versionAssemblyFile = 'AssemblyFileVersion("' + $version + '")';
+ 	
+	$updatedContent = get-content $assembly_info_file | % {$_ -replace $versionFilePattern, $versionAssemblyFile } | % {$_ -replace $versionPattern, $versionAssembly }
+	$updatedContent > $assembly_info_file
 }
 
 task clean {
