daae4e7f53bac0a4568939ea86811944c0bb676a
Arnold Zokas <arnold.zokas@coderoom.net>
2013-08-15 14:10:04 +0100

    Added script to push package to NuGet.

---
+++ build/push.ps1
@@ -0,0 +1,14 @@
+framework "4.0"
+
+task default -depends package
+
+formatTaskName {
+	param($taskName)
+	write-host
+	write-host (("-"*30) + "[ " + $taskName.ToUpper() + " ]" + ("-"*30)) -fore Green
+	write-host
+}
+
+task package {
+	& nuget push $package_file $api_key
+}
