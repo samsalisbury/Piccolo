7c82da9e92e8a15bfcc72d62f776a2cab1a23f91
Arnold Zokas <arnold.zokas@coderoom.net>
2013-08-19 11:34:48 +0100

    Tightened-up configuration API.

--- Piccolo/Configuration/HttpHandlerConfiguration.cs
+++ Piccolo/Configuration/HttpHandlerConfiguration.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using Piccolo.Request.ParameterBinders;
 using Piccolo.Routing;
 
@@ -9,9 +8,9 @@ namespace Piccolo.Configuration
 	public class HttpHandlerConfiguration
 	{
 		public IRequestHandlerFactory RequestHandlerFactory { get; set; }
-		public ReadOnlyCollection<Type> RequestHandlers { get; set; }
-		public Dictionary<Type, IParameterBinder> RouteParameterBinders { get; set; }
-		public IRequestRouter Router { get; set; }
+		public IList<Type> RequestHandlers { get; internal set; }
+		public Dictionary<Type, IParameterBinder> RouteParameterBinders { get; internal set; }
+		public IRequestRouter Router { get; internal set; }
 		public Func<object, string> JsonSerialiser { get; set; }
 		public Func<Type, string, object> JsonDeserialiser { get; set; }
 	}
