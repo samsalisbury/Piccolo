05e49f7bf1ee4dee1ede9135375d628528502a18
Arnold Zokas <arnold.zokas@coderoom.net>
2013-08-25 11:41:24 +0100

    Housekeeping.

--- Piccolo.Tasks.Site/js/app.js
+++ Piccolo.Tasks.Site/js/app.js
@@ -1,20 +1,20 @@
 'use strict';
 
 angular.module('piccoloClient', []).
-  config(['$routeProvider', function ($routeProvider) {
-  	$routeProvider.
-		when('/', { templateUrl: '/partials/tasks.html', controller: TaskListController }).
-		when('/:taskId', { templateUrl: '/partials/task.html', controller: TaskDetailController }).
-		otherwise({ redirectTo: '/' });
-  }]);
+	config(['$routeProvider', function($routeProvider) {
+		$routeProvider.
+			when('/', { templateUrl: '/partials/tasks.html', controller: TaskListController }).
+			when('/:taskId', { templateUrl: '/partials/task.html', controller: TaskDetailController }).
+			otherwise({ redirectTo: '/' });
+	}]);
 
 function TaskListController($scope) {
 	$scope.page = 1;
 	$scope.pageSize = 5;
-	
-	$scope.showTasks = function () {
-		$.getJSON("http://piccolo.com/tasks?pageNumber=" + $scope.page + "&pageSize=" + $scope.pageSize).done(function (data) {
-			$scope.$apply(function () {
+
+	$scope.showTasks = function() {
+		$.getJSON("http://piccolo.com/tasks?pageNumber=" + $scope.page + "&pageSize=" + $scope.pageSize).done(function(data) {
+			$scope.$apply(function() {
 				$scope.tasks = data.tasks;
 				$scope.totalCount = data.totalCount;
 
@@ -31,9 +31,9 @@ function TaskListController($scope) {
 		});
 	};
 
-	$scope.search = function () {
-		$.getJSON("http://piccolo.com/tasks/search?term=" + $scope.searchTerm + "&pageNumber=" + $scope.page + "&pageSize=" + $scope.pageSize).done(function (data) {
-			$scope.$apply(function () {
+	$scope.search = function() {
+		$.getJSON("http://piccolo.com/tasks/search?term=" + $scope.searchTerm + "&pageNumber=" + $scope.page + "&pageSize=" + $scope.pageSize).done(function(data) {
+			$scope.$apply(function() {
 				$scope.tasks = data.tasks;
 				$scope.totalCount = data.totalCount;
 
@@ -45,51 +45,51 @@ function TaskListController($scope) {
 			});
 		});
 	};
-	
-	$scope.getTask = function (id) {
+
+	$scope.getTask = function(id) {
 		var task;
 		$.ajax({
 			type: "GET",
 			url: "http://piccolo.com/tasks/" + id,
 			async: false
-		}).done(function (data) {
+		}).done(function(data) {
 			task = JSON.parse(data);
 		});
 		return task;
 	};
 
-	$scope.createTask = function (task) {
+	$scope.createTask = function(task) {
 		$.ajax({
 			type: "POST",
 			url: "http://piccolo.com/tasks",
 			data: JSON.stringify(task)
-		}).done(function () {
+		}).done(function() {
 			$scope.showTasks();
 		});
 	};
 
-	$scope.updateTask = function (task) {
+	$scope.updateTask = function(task) {
 		$.ajax({
 			type: "PUT",
 			url: "http://piccolo.com/tasks/" + task.id,
 			data: JSON.stringify(task)
-		}).done(function () {
+		}).done(function() {
 			$scope.showTasks();
 		});
 	};
 
-	$scope.deleteTask = function (id) {
+	$scope.deleteTask = function(id) {
 		$.ajax({
 			type: "DELETE",
 			url: "http://piccolo.com/tasks/" + id
-		}).done(function () {
+		}).done(function() {
 			$scope.showTasks();
 		});
 	};
 
 	$scope.tasks = $scope.showTasks();
-	
-	$scope.showPreviousPage = function () {
+
+	$scope.showPreviousPage = function() {
 		if ($scope.page > 1) {
 			$scope.page--;
 			$scope.showTasks();
@@ -103,21 +103,21 @@ function TaskListController($scope) {
 		}
 	};
 
-	$scope.showPage = function (pageNumber) {
+	$scope.showPage = function(pageNumber) {
 		$scope.page = pageNumber;
 		$scope.showTasks();
 	};
 
-	$scope.addTask = function () {
+	$scope.addTask = function() {
 		$scope.createTask({ title: $scope.newTaskTitle });
 		$scope.newTaskTitle = "";
 	};
 
-	$scope.removeTask = function (id) {
+	$scope.removeTask = function(id) {
 		$scope.deleteTask(id);
 	};
 
-	$scope.toggleCompletion = function (id, isCompleted) {
+	$scope.toggleCompletion = function(id, isCompleted) {
 		var task = $scope.getTask(id);
 		task.isCompleted = isCompleted;
 		$scope.updateTask(task);
@@ -126,17 +126,17 @@ function TaskListController($scope) {
 
 
 function TaskDetailController($scope, $routeParams) {
-	$scope.getTask = function (id) {
+	$scope.getTask = function(id) {
 		var task;
 		$.ajax({
 			type: "GET",
 			url: "http://piccolo.com/tasks/" + id,
 			async: false
-		}).done(function (data) {
+		}).done(function(data) {
 			task = JSON.parse(data);
 		});
 		return task;
 	};
-	
+
 	$scope.task = $scope.getTask($routeParams.taskId);
 }
