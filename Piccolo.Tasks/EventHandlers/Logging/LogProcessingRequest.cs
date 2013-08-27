e4f7b0d200fa995391425992292f2065aaef5ff9
Arnold Zokas <arnold.zokas@coderoom.net>
2013-08-27 12:48:49 +0100

    Implemented sample RequestProcessing event handler (#14).

---
+++ Piccolo.Tasks/EventHandlers/Logging/LogProcessingRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+using Piccolo.Events;
+
+namespace Piccolo.Tasks.EventHandlers.Logging
+{
+	public class LogProcessingRequest : IHandle<RequestProcessingEvent>
+	{
+		public void Handle(RequestProcessingEvent @event)
+		{
+			Trace.WriteLine("RequestProcessingEvent received.");
+		}
+	}
+}
