a854d2543ba132f163857ac9289326489acf8541
Arnold Zokas <arnold.zokas@coderoom.net>
2013-12-06 14:22:10 +0000

    Added tests for individual parameter binders.

--- Piccolo.Tests/Configuration/BootstrapperTests.cs
+++ Piccolo.Tests/Configuration/BootstrapperTests.cs
@@ -55,9 +55,45 @@ namespace Piccolo.Tests.Configuration
 			}
 
 			[Test]
-			public void it_should_configure_parameter_binders()
+			public void it_should_configure_string_parameter_binder()
 			{
-				_handlerConfiguration.ParameterBinders.Count.ShouldBeGreaterThan(0);
+				_handlerConfiguration.ParameterBinders[typeof(string)]("test").ShouldBe("test");
+			}
+
+			[Test]
+			public void it_should_configure_boolean_parameter_binder()
+			{
+				_handlerConfiguration.ParameterBinders[typeof(bool)]("true").ShouldBe(true);
+			}
+
+			[Test]
+			public void it_should_configure_nullable_boolean_parameter_binder()
+			{
+				_handlerConfiguration.ParameterBinders[typeof(bool?)]("true").ShouldBe(true);
+			}
+
+			[Test]
+			public void it_should_configure_integer_parameter_binder()
+			{
+				_handlerConfiguration.ParameterBinders[typeof(int)]("1").ShouldBe(1);
+			}
+
+			[Test]
+			public void it_should_configure_nullable_integer_parameter_binder()
+			{
+				_handlerConfiguration.ParameterBinders[typeof(int?)]("1").ShouldBe(1);
+			}
+
+			[Test]
+			public void it_should_configure_datetime_parameter_binder()
+			{
+				_handlerConfiguration.ParameterBinders[typeof(DateTime)]("01/01/1970 12:00:00").ShouldBe(new DateTime(1970, 01, 01, 12, 00, 00));
+			}
+
+			[Test]
+			public void it_should_configure_nullable_datetime_parameter_binder()
+			{
+				_handlerConfiguration.ParameterBinders[typeof(DateTime?)]("01/01/1970 12:00:00").ShouldBe(new DateTime(1970, 01, 01, 12, 00, 00));
 			}
 
 			[Test]
